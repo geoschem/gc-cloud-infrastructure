bcbdc6db448830c9cb69b1fdd9fc74ac82cbbf52
Liam Bindle <lrbindle@gmail.com>
2022-01-13 17:57:00 -0600

    added sed call to extract number of proc from runConfig.sh before running GCHP

--- benchmarks/scripts/stages/runGCHP.sh
+++ benchmarks/scripts/stages/runGCHP.sh
@@ -7,13 +7,19 @@ set -x
 : "${GEOSCHEM_BENCHMARK_STAGE_ARTIFACTS_DIR}"  # directory for stage artifacts
 : "${GEOSCHEM_BENCHMARK_SITE}"                 # which site is this running at
 
+
 cd run-directory
+
+# get number of processes
+num_proc=$(sed -n 's#TOTAL_CORES=\([0-9][0-9]*\)#\1#p' runConfig.sh)
+
+# launch GCHP
 case ${GEOSCHEM_BENCHMARK_SITE} in
     WUSTL)
-        mpirun -np 72 ./gchp
+        mpiexec -n ${num_proc} ./gchp
         ;;
     AWS)
-        mpirun --allow-run-as-root --hostfile hostfile.txt -np 72 ./gchp
+        mpirun --allow-run-as-root --hostfile hostfile.txt -np ${num_proc} ./gchp
         ;;
     *)
         >&2 echo "error: unknown site '${GEOSCHEM_BENCHMARK_SITE}' (GEOSCHEM_BENCHMARK_SITE)"
