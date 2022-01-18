30b378c82fc0e1c7df19d970780a1c48a5c85266
Liam Bindle <lrbindle@gmail.com>
2022-01-18 13:54:51 -0800

    first shot at dynamodb management

--- benchmarks/scripts/helpers/runStage.sh
+++ benchmarks/scripts/helpers/runStage.sh
@@ -4,60 +4,84 @@ export TMPDIR=${GEOSCHEM_BENCHMARK_TEMPDIR_PREFIX:-${TMPDIR}}  # prefix of tempo
 
 temp_dir=$(mktemp --directory)
 export GEOSCHEM_BENCHMARK_WORKING_DIR=${GEOSCHEM_BENCHMARK_WORKING_DIR:=${temp_dir}}    # working directory for this stage
-export GEOSCHEM_BENCHMARK_STAGE_ARTIFACTS_DIR=$(mktemp --directory)                     # directory whose contents are saved as artifacts
 log_file=$(mktemp)
 
 set -e  # exit on any error
 set -u  # treat undefined variables as an error
+set -x
 
-: "${GEOSCHEM_BENCHMARK_S3_BUCKET}" "${GEOSCHEM_BENCHMARK_INSTANCE_ID}"
+: "${GEOSCHEM_BENCHMARK_S3_BUCKET}" "${GEOSCHEM_BENCHMARK_INSTANCE_ID}" "${GEOSCHEM_BENCHMARK_TABLE_NAME}"
 
 stage_script=$(realpath $1)  # script that will be executed
 stage_short_name=$2          # name of this stage (one word)
 
-s3_artifacts_dir=${GEOSCHEM_BENCHMARK_S3_BUCKET}/${GEOSCHEM_BENCHMARK_INSTANCE_ID}      # s3 path to artifact files
-stage_artifact_filename=${GEOSCHEM_BENCHMARK_INSTANCE_ID}-${stage_short_name}.tar.gz    # file name of this stage's artifact file
+s3_artifacts_dir=${GEOSCHEM_BENCHMARK_S3_BUCKET}/${GEOSCHEM_BENCHMARK_INSTANCE_ID}/Artifacts    # s3 path to artifact files
+s3_logs_dir=${GEOSCHEM_BENCHMARK_S3_BUCKET}/${GEOSCHEM_BENCHMARK_INSTANCE_ID}/Logs
 
 function stage_has_already_run() {
-    aws s3 ls ${s3_artifacts_dir}/${stage_artifact_filename} &> /dev/null
+    aws dynamodb get-item \
+        --table-name ${GEOSCHEM_BENCHMARK_TABLE_NAME} \
+        --key "{\"InstanceID\": {\"S\": \"${GEOSCHEM_BENCHMARK_INSTANCE_ID}\"}}" \
+        --attributes-to-get "StagesCompleted" | jq -e ".Item.StagesCompleted.L[] | select(.S == \"${stage_short_name}\")" &> /dev/null
 }
 
 function download_artifacts() {
     if aws s3 ls ${s3_artifacts_dir} &> /dev/null ; then
-        aws s3 cp ${s3_artifacts_dir}/ . --recursive --exclude "*" --include "${GEOSCHEM_BENCHMARK_INSTANCE_ID}-*.tar.gz" --only-show-errors
-        for artifact_file in *.tar.gz; do
-            [ -e ${artifact_file} ] || continue
-            tar -xvzf ${artifact_file}
-        done
+        aws s3 cp ${s3_artifacts_dir}/ . --recursive --only-show-errors
     fi
 }
 
 function upload_artifacts() {
-    tar -cvzf ${stage_artifact_filename} --directory=${GEOSCHEM_BENCHMARK_STAGE_ARTIFACTS_DIR} .
-    aws s3 cp ${stage_artifact_filename} ${s3_artifacts_dir}/${stage_artifact_filename} --only-show-errors
-    rm -f ${stage_artifact_filename}
+    for file_path in "$@"; do
+        aws s3 cp ${file_path} ${s3_artifacts_dir}/${file_path} --only-show-errors
+    done
+}
+export -f upload_artifacts
+
+
+function register_stage_completed() {
+    aws dynamodb update-item \
+        --table-name ${GEOSCHEM_BENCHMARK_TABLE_NAME} \
+        --key "{\"InstanceID\": {\"S\": \"${GEOSCHEM_BENCHMARK_INSTANCE_ID}\"}}" \
+        --update-expression "SET #s = list_append(#s, :vals)" \
+        --expression-attribute-names '{"#s": "StagesCompleted"}' \
+        --expression-attribute-values "{\":vals\": {\"L\": [ { \"S\": "${s3_log_file_name}" }]}}"
 }
 
-function upload_log_file() {
-    aws s3 cp ${log_file} ${s3_artifacts_dir}/logs/${stage_short_name}.txt --only-show-errors
-}
-
-if ! stage_has_already_run; then
-    echo "Running stage '${stage_short_name}'"
-
-    # subshell so we return to cwd afterwards
+function exec_stage() {
     (
         set -x  # print commands to log file
         cd ${GEOSCHEM_BENCHMARK_WORKING_DIR}
         download_artifacts
         
         set -o pipefail        
-        ${stage_script} 2>&1 | tee ${log_file} && upload_artifacts
+        ${stage_script} 2>&1 | tee ${log_file}
         set +o pipefail
     )
+}
 
-    upload_log_file
+function upload_log_file() {
+    s3_log_file_name=${s3_logs_dir}/${stage_short_name}.txt
+    aws s3 cp ${log_file} ${s3_log_file_name} --only-show-errors
+    aws dynamodb update-item \
+        --table-name ${GEOSCHEM_BENCHMARK_TABLE_NAME} \
+        --key "{\"InstanceID\": {\"S\": \"${GEOSCHEM_BENCHMARK_INSTANCE_ID}\"}}" \
+        --update-expression "SET #s = list_append(#s, :vals)" \
+        --expression-attribute-names '{"#s": "LogFiles"}' \
+        --expression-attribute-values "{\":vals\": {\"L\": [ { \"S\": "${s3_log_file_name}" }]}}"
+}
+
+if ! stage_has_already_run; then
+    echo "Running stage '${stage_short_name}'"
+
+    if exec_stage ; then
+        register_stage_completed
+        upload_log_file
+    else
+        upload_log_file
+        exit 1
+    fi
 else 
     echo "Stage '${stage_short_name}' is already complete"
 fi
-rm -rf ${temp_dir} ${GEOSCHEM_BENCHMARK_STAGE_ARTIFACTS_DIR} ${log_file}
+rm -rf ${temp_dir} ${log_file}
