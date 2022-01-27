05c5f01783765beb1819559f031e4b64d1b97328
Liam Bindle <lrbindle@gmail.com>
2022-01-27 17:46:34 -0600

    Added README, removed some old stuff from runGCHP.sh; added resource files to stages

--- benchmarks/scripts/stages/runGCHP.sh
+++ benchmarks/scripts/stages/runGCHP.sh
@@ -9,17 +9,6 @@ set -x
 : "${GEOSCHEM_BENCHMARK_WORKING_DIR}"  # working directory
 : "${GEOSCHEM_BENCHMARK_SITE}"         # which site is this running at
 
-
-function wustl_create_temporary_extdata() {
-    (
-        mkdir -p ${GEOSCHEM_BENCHMARK_EXTDATA_DIR}
-        cd ${GEOSCHEM_BENCHMARK_EXTDATA_DIR}
-        bashdatacatalog ${GEOSCHEM_BENCHMARK_CATALOG_FILES} fetch
-        bashdatacatalog ${GEOSCHEM_BENCHMARK_CATALOG_FILES} list-missing relative 2019-06-30 2019-07-02 > temp-extdata-files.txt
-        rsync -avip --link-dest=/storage1/fs1/rvmartin/Active/GEOS-Chem-shared/ExtData/ --files-from=temp-extdata-files.txt /storage1/fs1/rvmartin/Active/GEOS-Chem-shared/ExtData/ ./
-    )
-}
-
 cd run-directory
 
 # get number of processes
@@ -28,7 +17,6 @@ num_proc=$(sed -n 's#TOTAL_CORES=\([0-9][0-9]*\)#\1#p' runConfig.sh)
 # launch GCHP
 case ${GEOSCHEM_BENCHMARK_SITE} in
     WUSTL)
-        #[ ! -z "${GEOSCHEM_BENCHMARK_CATALOG_FILES}" ] && wustl_create_temporary_extdata
         export TMPDIR="$__LSF_JOB_TMPDIR__"
         chmod +x ./gchp
         mpiexec -n ${num_proc} ./gchp
